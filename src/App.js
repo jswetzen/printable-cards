d18cebd0904baaf95da68894d11e094a121fd4e3
Johan Swetzén <johan@greenbyte.com>
2020-04-05 16:51:57 +0200

    Printable Cards app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Book.css';
+import { cards } from './Cards.js';
+
+// Detailed card descriptions at
+// https://hackernoon.com/35-cards-which-will-improve-your-backlog-refinement-process-and-engage-every-team-member-54f929fdd282
+
+class Card extends React.Component {
+  render () {
+    const card = this.props.card;
+    return (
+      <div className="card">
+        <span className="card-top-number">{card.number}</span>
+        <div className="card-title">{card.title}</div>
+        <div className="card-description">{card.text}</div>
+        <div className="card-bottom-number">{card.number}</div>
+      </div>
+    );
+  }
+}
+
+class Page extends React.Component {
+  render () {
+    const cardsPerRow = 3;
+    var rows = [];
+    for(let i=0, j=this.props.cards.length; i < j; i += cardsPerRow) {
+      let cards = [];
+      this.props.cards.slice(i, i + cardsPerRow).forEach((card, k) => {
+        cards.push(<Card key={i+k} card={card} />);
+                 });
+        rows.push(
+          <header className="table-row" key={'row'+i}>
+            {cards}
+          </header>
+        );
+    }
+    return (
+      <div className="page">
+        <div>
+          {rows}
+          </div>
+      </div>
+    );
+  }
+}
+
+class Book extends React.Component {
+  render() {
+    const cards = this.props.cards;
+    const cardsPerPage = 9;
+    var pages = [];
+    for(let i=0, j=cards.length; i < j; i += cardsPerPage) {
+      pages.push(<Page key={i} cards={cards.slice(i, i + cardsPerPage)} />);
+    }
+    return (
+      <div className="book">
+        {pages}
+      </div>);
+  }
+}
+
+//window.print();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Book cards={cards} />;
 }
 
 export default App;
---
+++ src/Cards.js
@@ -0,0 +1,64 @@
+const cards = [
+  {
+    number: 1,
+    title: "Small / Low",
+    text: "Hej  ",
+  },
+  {
+    number: 2,
+    title: "Medium",
+    text: "",
+  },
+  {
+    number: 3,
+    title: "Large / High",
+    text: "",
+  },
+  {
+    number: 1,
+    title: "Small / Low",
+    text: "",
+  },
+  {
+    number: 2,
+    title: "Medium",
+    text: "",
+  },
+  {
+    number: 3,
+    title: "Large / High",
+    text: "",
+  },
+  {
+    number: 1,
+    title: "Small / Low",
+    text: "",
+  },
+  {
+    number: 2,
+    title: "Medium",
+    text: "",
+  },
+  {
+    number: 3,
+    title: "Large / High",
+    text: "",
+  },
+  {
+    number: 4,
+    title: "Large / High",
+    text: "",
+  },
+  {
+    number: 5,
+    title: "Large / High",
+    text: "",
+  },
+  {
+    number: 6,
+    title: "Large / High",
+    text: "",
+  },
+];
+
+export { cards };
